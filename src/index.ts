80e69a5ad5e926639a85173223ee1025ff1758a0
faraz <farazmobile619@gmail.com>
2023-02-17 12:29:54 +0500

    init

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { cattleRouter } from "./cattle";
 import { milkYieldRouter } from "./milk_yield";
 import { apiRouter } from "./routes";
 
-const port = envConfig.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 
@@ -25,4 +25,4 @@ app.use('/',apiRouter)
 
 app.get("/", (req, res) => res.status(200).json({ message: "Server is running and up!" }));
 
-app.listen(port, () => console.log("server is running at port " + port));
+app.listen(Number(port),'0.0.0.0', () => console.log("server is running at port " + port));
