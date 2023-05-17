5d54c44734ab6d5138618c47d1e59be6a4e200bc
BurningIce <farazmobile619@gmail.com>
2023-05-17 12:31:34 +0500

    init

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,17 @@ import { apiRouter } from "./routes";
 const port = envConfig.PORT;
 
 const app = express();
-export const server = app.listen(Number(port), '0.0.0.0', () =>
-  console.log("server is running at port " + port)
-);
-// import "./socketServer";
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use("/api/chats", apiRouter);
-app.get("/",(req:Request,res:Response)=>{
+app.get("/", (req: Request, res: Response) => {
   return res.json({
-    message:"running"
-  })
-}
+    message: "running",
+  });
+});
 
-)
+app.listen(Number(port), "0.0.0.0", () =>
+  console.log("server is running at port " + port)
+);
+// import "./socketServer";
