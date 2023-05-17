3c9a6801b3c03139da7aabfd6e3363b62ceccccc
BurningIce <farazmobile619@gmail.com>
2023-05-17 13:00:16 +0500

    nn

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,28 @@ import express from "express";
 import cors from "cors";
 import { envConfig } from "./config/envConfig";
 import { apiRouter } from "./routes";
+import cookieParser from "cookie-parser";
 
-const port = envConfig.PORT;
+const port = envConfig.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.static("public"));
 app.use(cors({ origin: "*" }));
-app.use("/api/chats", apiRouter);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   return res.json({
     message: "running",
   });
 });
 
-app.listen(
-  Number(port),
-  envConfig.NODE_ENV == "development" ? "" : "0.0.0.0",
-  () => console.log("server is running at port " + port)
+app.use("/api/chats", apiRouter);
+
+app.listen(Number(port), "0.0.0.0", () =>
+  console.log("server is running at port " + port)
 );
 // import "./socketServer";
